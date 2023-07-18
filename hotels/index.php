ca6a56d93fc309dc89f5465e58546818e269c25f
GLCVLL <g.ioele96@outlook.it>
2023-07-18 15:50:31 +0200

    added table head

--- hotels/index.php
+++ hotels/index.php
@@ -10,3 +10,31 @@ foreach ($hotels as $hotel) {
     echo "Distanza dal centro: " . $hotel['distance_to_center'] . " km<br><br>";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' integrity='sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==' crossorigin='anonymous'/>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <h1>Hotel Information</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Parking</th>
+                    <th>Vote</th>
+                    <th>Distance to Center</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
